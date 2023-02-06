b6fe85a841262397864686d56cbb305de92f0aef
DESKTOP-8VJVGPD\admin <MinCheol>
2023-02-06 14:40:25 +0900

    플레이어 총, 총알

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,31 @@
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    [SerializeField] float _speed;
+    [SerializeField] float _lifeTime;
+    [SerializeField] int _damage;
+
+    void Update()
+    {
+        transform.Translate(Vector3.forward * Time.deltaTime * _speed);
+        Invoke("Remove", _lifeTime);
+    }
+
+    public void Remove()
+    {
+        Destroy(this.gameObject);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "Map")
+            Remove();
+        else if(collision.gameObject.tag == "Enemy")
+        {
+            collision.gameObject.GetComponent<Enemy>().TakeDamage(_damage);
+            Remove();
+        }
+    }
+}
