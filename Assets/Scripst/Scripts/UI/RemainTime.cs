421612ca26379799661cb0991720babd8256fd73
DESKTOP-8VJVGPD\admin <kjw>
2023-04-14 14:58:13 +0900

    더미샵 남은시간

---
+++ Assets/Scripst/Scripts/UI/RemainTime.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class RemainTime : MonoBehaviour
+{
+    Text text;
+    public static float _rTime = 300f;
+    void Start()
+    {
+        text=GetComponent<Text>();
+    }
+
+  
+    void Update()
+    {
+        _rTime -= Time.deltaTime;
+        if (_rTime < 0)
+            _rTime = 0;
+        text.text= "남은 시간 :" + Mathf.Round(_rTime);
+    }
+}
---
+++ Assets/Scripst/Scripts/UI/dummyShop.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class dummyShop : MonoBehaviour
+{
+    public RectTransform uiGroup;
+    public Animator anim;
+
+    int player;
+    public GameObject[] itemObj;
+    public int[] itemPrice;
+    public Transform[] itemPos;
+    public string[] talkData;
+    public Text talkText;
+
+    Playerdummy enterPlayer;
+    public void Enter(Playerdummy playerdummy)
+    {
+        enterPlayer = playerdummy;
+        uiGroup.anchoredPosition = Vector3.zero;
+    }
+
+
+    public void Exit()
+    {
+        //anim.SetTrigger("doHello");
+        uiGroup.anchoredPosition = Vector3.down * 1500;
+    }
+
+    public void Buy(int index)
+    {
+        int price = itemPrice[index];
+        if (price > enterPlayer._coin)
+        {
+            StopCoroutine(Talk());
+            StartCoroutine(Talk());
+            return;
+        }
+
+        enterPlayer._coin -= price;
+        Vector3 ranVec = Vector3.right * Random.Range(-3, 3) + Vector3.forward * Random.Range(-3, 3);
+        Instantiate(itemObj[index], itemPos[index].position + ranVec, itemPos[index].rotation);
+
+    }
+    IEnumerator Talk()
+    {
+        talkText.text = talkData[1];
+        yield return new WaitForSeconds(2f);
+        talkText.text = talkData[0];
+    }
+}
