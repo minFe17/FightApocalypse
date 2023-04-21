3cd751eff201bd583717ee15e25e81e436ae4972
Nimblebody <62288670+Nimblebody@users.noreply.github.com>
2023-04-21 15:15:24 +0900

    몬스터 체력바 테스트 제작

--- Assets/Scripst/PlayerTest.cs
+++ Assets/Scripst/PlayerTest.cs
@@ -10,40 +10,54 @@ public class PlayerTest : MonoBehaviour
     [SerializeField] private float cameraRotationLimit;
     private float currentCameraRotationX = 0;
     [SerializeField] private Camera theCamera;
-     private Rigidbody MyRigid;
+    private Rigidbody MyRigid;
     // Start is called before the first frame update
+
+
     void Start()
     {
-       
-       MyRigid= GetComponent<Rigidbody>();  
+        theCamera = FindObjectOfType<Camera>();
+        MyRigid = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
         Move();
+        CharacterRotation();
         CameraRotation();
     }
-    private void Move()
+
+    public void Move()
     {
-        float _moveDirX = Input.GetAxisRaw("Horizontal");
-        float _moveDirZ = Input.GetAxisRaw("Vertical");
-        Vector3 _moveHorizontal = transform.right* _moveDirX;   
-        Vector3 _moveVertical = transform.forward * _moveDirZ;
+        float _moveDirx = Input.GetAxisRaw("Horizontal");
+        float _moveDirz = Input.GetAxisRaw("Vertical");
 
-        Vector3 _velocity = (_moveHorizontal + _moveVertical).normalized *walkSpeed;
+        Vector3 _moveHorizontal = transform.right * _moveDirx;
+        Vector3 _moveVertical = transform.forward * _moveDirz;
 
-        MyRigid.MovePosition(transform.position+_velocity*Time.deltaTime);
+        Vector3 _velocity = (_moveHorizontal + _moveVertical).normalized * walkSpeed;
+
+        MyRigid.MovePosition(transform.position + _velocity * Time.deltaTime);
+
+
     }
-    private void CameraRotation()
+    public void CharacterRotation()
     {
-        Debug.Log("Ä«¸Þ¶ó");
-        float _xRotation = Input.GetAxisRaw("Mouse Y");
-        float _cameraRotarionX = _xRotation * lookSensitivity;
-       currentCameraRotationX += _cameraRotarionX;
-        currentCameraRotationX = Mathf.Clamp(currentCameraRotationX, -cameraRotationLimit, cameraRotationLimit);
+        float _yRotation = Input.GetAxisRaw("Mouse X");
+        Vector3 _characterRotationY = new Vector3(0f, _yRotation, 0f) * lookSensitivity;
+        MyRigid.MoveRotation(MyRigid.rotation * Quaternion.Euler(_characterRotationY));
+        Debug.Log(MyRigid.rotation);
+        Debug.Log(MyRigid.rotation.eulerAngles);
+    }
 
-        theCamera.transform.localEulerAngles = new Vector3(currentCameraRotationX,0f,0f);   
+    public void CameraRotation()
+    {
+        float _xRotation = Input.GetAxisRaw("Mouse Y");
+        float _cameraRotationX = _xRotation * lookSensitivity;
+        currentCameraRotationX -= _cameraRotationX;
+        currentCameraRotationX = Mathf.Clamp(currentCameraRotationX, _cameraRotationX, cameraRotationLimit);
+
+        theCamera.transform.localEulerAngles = new Vector3(currentCameraRotationX, 0f, 0f);
     }
 }
-
